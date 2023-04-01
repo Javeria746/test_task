2ebbcae6eb811505ae712f7a9b33cd77eadaf1d5
javeria746 <javeriashaikh746@gmail.com>
2023-04-02 00:26:31 +0500

    design-modification

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,12 @@ import { ToastrModule } from 'ngx-toastr';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContactListComponent } from './contact-list/contact-list.component';
-import { RegisterFormComponent } from './register-form/register-form.component';
 import { LoginFormComponent } from './login-form/login-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContactListComponent,
-    RegisterFormComponent,
     LoginFormComponent
   ],
   imports: [
