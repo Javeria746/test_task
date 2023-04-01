2a130f0d57748e30284581745b7db89a95651c5f
javeria746 <javeriashaikh746@gmail.com>
2023-04-01 18:14:01 +0500

    login-functionality

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactListComponent } from './contact-list/contact-list.component';
+import { LoginFormComponent } from './login-form/login-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:'/login', pathMatch:'full' },
+  { path: 'login', component: LoginFormComponent },
+  { path: 'contact-list', component: ContactListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/login-form/login-form.component.ts
+++ src/app/login-form/login-form.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-login-form',
   templateUrl: './login-form.component.html',
   styleUrls: ['./login-form.component.css']
 })
-export class LoginFormComponent {
+export class LoginFormComponent implements OnInit {
+signupUsers : any[] =[];
+signupObj : any = {
+  userName:'',
+  email:'',
+  password:''
 
+};
+loginObj : any = {
+  userName:'',
+  password:''
+
+};
+  constructor(private router :Router){
+
+}
+  ngOnInit(): void {
+const localData = localStorage.getItem('signUpUsers');
+if(localData != null){
+  this.signupUsers = JSON.parse(localData);
+}
+}
+onSignUp(){
+this.signupUsers.push(this.signupObj);
+localStorage.setItem('signUpUsers', JSON.stringify(this.signupUsers));
+this.signupObj = {
+  userName:'',
+  email:'',
+  password:''
+}
+}
+onLogin(){
+const isUserExist = this.signupUsers.find(res => res.userName == this.loginObj.userName && res.password == this.loginObj.password)
+if(isUserExist != undefined){
+  alert("user logged in successfully");
+  this.router.navigate(['/contact-list']);
+}
+else {
+  alert('user not logged in')
+}
+}
 }
