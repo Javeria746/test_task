d9b4b7f9f5a4a7c49898e267e34b9eab37b04a2b
javeria746 <javeriashaikh746@gmail.com>
2023-04-02 00:23:25 +0500

    design-modification

--- src/app/contact-list/contact-list.component.ts
+++ src/app/contact-list/contact-list.component.ts
@@ -10,9 +10,9 @@ declare var window:any;
 export class ContactListComponent implements OnInit {
   btn:any='Save';
   formModal: any;
-  studentArr: any[] = [];
-  student : any = {
-    studentID:0,
+  contactList: any[] = [];
+  contact : any = {
+    contactID:0,
     name:'',
     email:'',
     mobile:'',
@@ -23,16 +23,18 @@ constructor(private toast:ToastrService){
 
 }
 ngOnInit(): void {
+  this.btn='Save';
 this.formModal = new window.bootstrap.Modal(document.getElementById("exampleModal"))
-const localdata = localStorage.getItem('studentList');
+const localdata = localStorage.getItem('contactList');
 if(localdata != null){
-  this.studentArr = JSON.parse(localdata);
+  this.contactList = JSON.parse(localdata);
 }
 }
 openmodal(){
+
 this.formModal.show();
-this.student = {
-  studentID:0,
+this.contact = {
+  contactID:0,
   name:'',
   email:'',
   mobile:'',
@@ -45,14 +47,15 @@ this.formModal.hide();
 }
 saveData(data:any){
   debugger;
+
   if(this.btn=='Save'){
-    this.student.studentID = this.studentArr.length+1;
-    this.studentArr.push(this.student);
+    this.contact.contactID = this.contactList.length+1;
+    this.contactList.push(this.contact);
     this.closeModal();
     this.toast.success('Added Successfully!');
-    localStorage.setItem('studentList', JSON.stringify(this.studentArr));
-    this.student = {
-      studentID:0,
+    localStorage.setItem('contactList', JSON.stringify(this.contactList));
+    this.contact = {
+      contactID:0,
       name:'',
       email:'',
       mobile:'',
@@ -61,36 +64,36 @@ saveData(data:any){
     }
   }
   else if(this.btn=='update'){
-    const record = this.studentArr.find(res=> res.studentID == this.student.studentID);
-   record.name = this.student.name;
-   record.email =this.student.email;
-   record.mobile =this.student.mobile;
-   record.description = this.student.description;
+    const record = this.contactList.find(res=> res.contactID == this.contact.contactID);
+   record.name = this.contact.name;
+   record.email =this.contact.email;
+   record.mobile =this.contact.mobile;
+   record.description = this.contact.description;
   
    this.closeModal();
    this.toast.success('updated  Successfully!');
-   localStorage.setItem('studentList', JSON.stringify(this.studentArr));
+   localStorage.setItem('contactList', JSON.stringify(this.contactList));
 
   }
  
 }
-onEdit(student:any){
+onEdit(contact:any){
  
-  this.btn= 'update'
+  this.btn='update';
   this.openmodal();
 
-  this.student = student;
+  this.contact = contact;
 
 
 }
 onDelete(id:any){
-  for (let i =0; i < this.studentArr.length; i++){
-    if(this.studentArr[i].studentID == id){
+  for (let i =0; i < this.contactList.length; i++){
+    if(this.contactList[i].contactID == id){
       
-      this.studentArr.splice(i,1)
+      this.contactList.splice(i,1)
     }
   }
-  localStorage.setItem('studentList', JSON.stringify(this.studentArr));
+  localStorage.setItem('contactList', JSON.stringify(this.contactList));
   this.toast.error("record deleted successfully!")
   
 }
